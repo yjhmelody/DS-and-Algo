86881b5999d43450515474939b57743a9471fd00
yjhmelody <465402634@qq.com>
2018-03-07 19:19:53 +0800

    update

--- PAT/1001.cpp
+++ PAT/1001.cpp
@@ -14,17 +14,18 @@ int main() {
   ss >> str;
 
   string str_sum;
-  for (int i = str.length()-1, n = 1; i >= 0; i--, n++) {
+  for (int i = str.length() - 1, n = 1; i >= 0; i--, n++) {
     str_sum.append(&str[i], 1);
     if (n == 3) {
       n = 0;
-      if (str[i-1] != '-') {
+      if (str[i - 1] != '-') {
         str_sum.append(",");
       }
     }
   }
 
-  int i = str_sum[str_sum.length()-1] != ',' ? str_sum.length()-1 : str_sum.length()-2;
+  int i = str_sum[str_sum.length() - 1] != ',' ? str_sum.length() - 1
+                                               : str_sum.length() - 2;
   for (; i >= 0; i--) {
     cout << str_sum[i];
   }
---
+++ PAT/1002.cpp
@@ -0,0 +1,93 @@
+#include <algorithm>
+#include <cstdio>
+#include <iostream>
+#include <iterator>
+#include <string>
+#include <utility>
+#include <vector>
+
+using namespace std;
+
+int main() {
+  int k1, k2;
+
+  cin >> k1;
+  vector<int> a;
+  vector<double> coe1;
+  for (int i = 0; i < k1; i++) {
+    int temp_exp;
+    double temp_coe;
+    cin >> temp_exp;
+    cin >> temp_coe;
+    a.push_back(temp_exp);
+    coe1.push_back(temp_coe);
+  }
+
+  cin >> k2;
+  vector<int> b;
+  vector<double> coe2;
+  for (int i = 0; i < k2; i++) {
+    int temp_exp;
+    double temp_coe;
+    cin >> temp_exp;
+    cin >> temp_coe;
+    b.push_back(temp_exp);
+    coe2.push_back(temp_coe);
+  }
+
+  vector<int> sum;
+  vector<double> coe3;
+
+  auto it = a.begin();
+  auto it2 = b.begin();
+  auto it_coe1 = coe1.begin();
+  auto it_coe2 = coe2.begin();
+  while (it != a.end() && it2 != b.end()) {
+    if (*it > *it2) {
+      sum.push_back(*it);
+      coe3.push_back(*it_coe1);
+      it++;
+      it_coe1++;
+    } else if (*it < *it2) {
+      sum.push_back(*it2);
+      coe3.push_back(*it_coe2);
+      it2++;
+      it_coe2++;
+    } else {
+      if (*it_coe1 + *it_coe2 != 0.0) {
+        sum.push_back(*it);
+        coe3.push_back(*it_coe1 + *it_coe2);
+      }
+      it++;
+      it_coe1++;
+      it2++;
+      it_coe2++;
+    }
+  }
+
+  for (; it != a.end(); it++, it_coe1++) {
+    sum.push_back(*it);
+    coe3.push_back(*it_coe1);
+  }
+
+  for (; it2 != b.end(); it2++, it_coe2++) {
+    sum.push_back(*it2);
+    coe3.push_back(*it_coe2);
+  }
+
+  cout << sum.size() << " ";
+  auto it_coe3 = coe3.begin();
+  for (auto it = sum.begin(); it != sum.end() - 1; it++, it_coe3++) {
+    if (*it_coe3 == 0.0) {
+      continue;
+    }
+    if (it + 1 == sum.end()) {
+      printf("%d %.1f\n", *it, *it_coe3);
+      break;
+    }
+    printf("%d %.1f ", *it, *it_coe3);
+  }
+
+  // 有一个测试用例无法通过
+  return 0;
+}
