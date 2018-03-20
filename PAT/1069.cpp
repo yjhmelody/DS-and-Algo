3f79c08a44844aa9b9df6f1c8146c17541a63c00
yjhmelody <465402634@qq.com>
2018-03-20 12:46:53 +0800

    finish the 1069

--- PAT/1069.cpp
+++ PAT/1069.cpp
@@ -2,42 +2,45 @@
 #include<vector>
 #include<algorithm>
 #include<functional>
+#include<cstdio>
 
 using namespace std;
 
 int reorder(int num, bool is_inc = true, int n = 4) {
 	vector<int> vec(n);
-   for(int i = 0; i < n; i++) {
-   		vec.push_back(num % 10);
-      num /= 10;
-   }
-   if(is_inc) {
+    for(auto &x: vec) {
+   		x = num % 10;
+        num /= 10;
+    }
+    if(is_inc) {
    		sort(vec.begin(), vec.end());
-   	}
-   else {
+    }
+    else {
    		sort(vec.begin(),vec.end(), greater<int>());
-   }
-   
-   int sum = 0;
-   for(auto &x: vec) {
+    }
+
+    int sum = 0;
+    for(auto &x: vec) {
    		sum = sum * 10 + x;
-   }
-   return sum;
+    }
+    return sum;
 }
 
 int main() {
-	int num;
-   cin >> num;
-   int a = reorder(num, false);
-   int b = reorder(num, true);
-   if(a == b) {
-   		cout << a << " - " << b << " = 0";		return 0;
-   }
-   int c = num;
-   do {
+    int num;
+    cin >> num;
+    int a = reorder(num, false);
+    int b = reorder(num, true);
+    if(a == b) {
+        cout << a << " - " << b << " = 0000";
+   		return 0;
+    }
+    int c = num;
+    do {
    		a = reorder(c, false);
-      b = reorder(c, true);
-   		cout << a << " - " << b << " = " << c << endl;
-   } while(c != 6174);
-	return 0;
+        b = reorder(c, true);
+        c = a - b;
+   		printf("%04d - %04d = %04d\n", a, b, c);
+    } while(c != 6174);
+    return 0;
 }
