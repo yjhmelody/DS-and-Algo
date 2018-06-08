38e2a0b8bd0c263b1bd581a4f104c6e025b4d602
yjhmelody <465402634@qq.com>
2018-06-09 00:05:50 +0800

    haskell: BST

--- other/AlgoXY/Haskell/Tree/BST.hs
+++ other/AlgoXY/Haskell/Tree/BST.hs
@@ -65,3 +65,57 @@ delete (Node l x r) k   | k < x = (Node (delete l k) x r)
                         | otherwise = (Node l x' (delete r x'))
                         where x' = min r
 
+
+
+-- Traverse a part of tree inside a range [a, b]
+mapR :: (Ord a) => (a -> b) -> a -> a -> Tree a -> Tree b
+mapR f a b tree = map' tree
+    where
+        map' Empty = Empty
+        map' (Node l k r)   | k < a = map' r
+                            | k >= a && k <= b = Node (map' l) (f k) (map' r)
+                            | k > b = map' l
+
+toList :: (Ord a) => Tree a -> [a]
+toList Empty = []
+toList (Node l x r) = toList l ++ [x] ++ toList r 
+
+fromList :: (Ord a) => [a] -> Tree a
+fromList = foldl insert Empty
+
+-- Node (Node Empty 1 Empty) 2 (Node Empty 3 Empty)
+
+-- test
+prop_build :: (Show a, Ord a) => [a] -> Bool
+prop_build xs = L.sort xs == (toList $ fromList xs)
+
+prop_map :: (Ord a) => [a] -> Bool
+prop_map xs = mapT id (fromList xs) == fromList xs
+
+prop_lookup :: (Ord a) => [a] -> a -> Bool
+prop_lookup xs x = f $ key $ lookup (fromList xs) x 
+    where
+        f Nothing  = not $ elem x xs
+        f (Just y) = x == y
+
+prop_min :: (Ord a, Num a) => [a] -> Property
+prop_min xs = not (null xs) ==> minimum xs == min (fromList xs)
+
+prop_max :: (Ord a, Num a) => [a] -> Property
+prop_max xs = not (null xs) ==> maximum xs == max (fromList xs)
+
+prop_del :: (Ord a, Num a) => [a] -> a -> Bool
+prop_del xs x = L.sort (L.delete x xs) == toList (delete (fromList xs) x)
+
+prop_mapR :: (Ord a, Num a) =>[a] -> a -> a -> Bool
+prop_mapR xs a b = filter (\x-> a<= x && x <=b) (L.sort xs) ==
+                   toList (mapR id a b (fromList xs))
+
+testAll = do
+    quickCheck (prop_build :: [Int] -> Bool)
+    quickCheck (prop_map :: [Int] -> Bool)
+    quickCheck (prop_lookup:: [Int] -> Int -> Bool)
+    quickCheck (prop_min:: [Int] -> Property)
+    quickCheck (prop_max:: [Int] -> Property)
+    quickCheck (prop_del:: [Int] -> Int -> Bool)
+    quickCheck (prop_mapR:: [Int] -> Int -> Int -> Bool)
